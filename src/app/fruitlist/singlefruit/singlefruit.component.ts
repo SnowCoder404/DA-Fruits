a393a5d8d2c28181103a7cd94bb3cdcb86955c49
SnowCoder404 <lenschaumarco@protonmail.com>
2025-03-02 00:21:09 +0100

    Add single fruit component

---
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -0,0 +1,13 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-singlefruit',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './singlefruit.component.html',
+  styleUrl: './singlefruit.component.scss'
+})
+export class SinglefruitComponent {
+
+}
