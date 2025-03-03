c0bc20a13c08852b4cc5c48d48ba7ba638790c1e
SnowCoder404 <lenschaumarco@protonmail.com>
2025-03-03 16:50:07 +0100

    Test

--- src/app/fruitlist/singlefruit/singlefruit.component.ts
+++ src/app/fruitlist/singlefruit/singlefruit.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-singlefruit',
@@ -9,5 +9,12 @@ import { Component } from '@angular/core';
   styleUrl: './singlefruit.component.scss'
 })
 export class SinglefruitComponent {
-
+  @Input()fruit = {
+    name: "Apfel",
+    img:"apple.png",
+    description: "Äpfel sind aufgrund ihres hohen Wassergehalts kalorienarm und enthalten nur Spuren von Fett und Eiweiß, dafür aber rund zwei Prozent Ballaststoffe und etwa elf Prozent Kohlenhydrate. Äpfel enthalten auch viele Vitamine und Mineralstoffe und sind daher eine wichtige Quelle für uns - zum Beispiel für Vitamin C.",
+    genus: "Kernobstgewächsen innerhalb der Familie der Rosengewächse",
+    stars: 2.3,
+    reviews:[{name: "Kevin W.", text: "ist lecker"},{name: "Arne P.", text: "nicht so meins"}],
+  };
 }
